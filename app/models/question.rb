5ee886977fcf5939ee63fc7050deda0d5b2d7576
Valerie Campbell <valeriecs10@gmail.com>
2020-11-17 13:27:18 -0800

    add results methods to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,4 +10,32 @@ class Question < ApplicationRecord
     class_name: :Poll,
     foreign_key: :poll_id,
     primary_key: :id
+
+  def n_plus_1_results
+    answers = self.answer_choices
+    answers_count = {}
+
+    answers.each do |answer|
+      answers_count[answer.text] = answer.responses.length
+    end
+    answers_count
+  end
+
+  def better_results
+    answers = self.answer_choices.includes(:responses)
+    answers_count = {}
+
+    answers.each do |answer|
+      answers_count[answer.text] = answer.responses.length
+    end
+    answers_count
+  end
+
+  def best_results
+    AnswerChoice
+      .select('answer_choices.*, COUNT(responses.id) AS total')
+      .left_outer_joins(:responses)
+      .where('answer_choices.question_id = ?', self.id)
+      .group('answer_choices.id')
+  end
 end
