a5f4ed7c8441116c2ce2f24d79a7bc32eba9e231
Valerie Campbell <valeriecs10@gmail.com>
2020-11-18 13:56:42 -0800

    improved not_respond_to_own_poll validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -15,28 +15,34 @@ class Response < ApplicationRecord
     through: :answer_choice,
     source: :question
 
-    def sibling_responses
-      self.id.nil? ? self.question.responses : 
-        self.question.responses.where.not('responses.id = ?', self.id)
-    end
+  def sibling_responses
+    self.id.nil? ? self.question.responses : 
+      self.question.responses.where.not('responses.id = ?', self.id)
+  end
 
-    def respondent_already_answered?
-      sibling_responses.exists?(respondent_id: self.respondent_id)
-    end
+  def respondent_already_answered?
+    sibling_responses.exists?(respondent_id: self.respondent_id)
+  end
 
-    def not_duplicate_response
-      if respondent_already_answered?
-        errors[:user] << 'can\'t answer the same poll twice'
-      end
+  def not_duplicate_response
+    if respondent_already_answered?
+      errors[:user] << 'can\'t answer the same poll twice'
     end
+  end
 
-    def respondents_own_poll?
-      self.question.poll.author.id == self.respondent_id
-    end
+  def respondents_own_poll?
+    poll_author_id = Poll
+      .joins(questions: :answer_choices)
+      .where('answer_choices.id = ?', self.answer_choice_id)
+      .pluck('polls.author_id')
+      .first
 
-    def not_respond_to_own_poll
-      if respondents_own_poll?
-        errors[:user] << 'can\'t answer own poll'
-      end
+      poll_author_id == self.respondent_id
+  end
+
+  def not_respond_to_own_poll
+    if respondents_own_poll?
+      errors[:user] << 'can\'t answer own poll'
     end
+  end
 end
