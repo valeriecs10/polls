146248d3dc0b235182375b7d8200301ed6d9f0de
Valerie Campbell <valeriecs10@gmail.com>
2020-11-17 16:06:53 -0800

    add completed/incompleted_polls methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,34 @@ class User < ApplicationRecord
     class_name: :Response,
     foreign_key: :respondent_id,
     primary_key: :id 
+
+  def completed_polls
+    polls_with_completion_counts
+      .having('COUNT(DISTINCT questions.id) = COUNT(responses.id)')
+  end
+
+  def incomplete_polls
+    polls_with_completion_counts
+      .having('COUNT(DISTINCT questions.id) > COUNT(responses.id)')
+      .having('COUNT(responses.id > 0')
+  end
+
+  private
+
+  def polls_with_completion_counts
+    joins_sql = <<-SQL 
+      LEFT OUTER JOIN (
+        SELECT
+          *
+        FROM
+          responses
+        WHERE
+          respondent_id = #{self.id}
+      ) AS responses ON answer_choices.id = responses.answer_choice_id
+    SQL
+
+    Poll.joins(questions: :answer_choices)
+      .joins(joins_sql)
+      .group('polls.id')
+  end
 end
